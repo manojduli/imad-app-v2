022c4e63edf8819242c00a36c9873d355dfd4313
manoj duli <manojduli123@gmail.com>
2017-03-17 23:30:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/article-one', function(req,res){
  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/article-two', function(req,res){
+ res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+
 app.get('/article-two', function(req, res){
     res.send('article two is served here');
 });
