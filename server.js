20c976ddc0b1815843df48657153635c8de067d8
manoj duli <manojduli123@gmail.com>
2017-03-18 18:32:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,71 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+   title:"article-one",
+   heading:"article-one on cyber security",
+   date:"March 3rd 2017",
+   content:`
+   
+                <h1 id="title"> <i>Cyber security:</i></h1>
+                <h5 id="section">
+                    Computer security, also known as cybersecurity or IT security, is the protection of computer systems from the theft or damage to the hardware, software or the information on them, as well as from disruption or misdirection of the services they provide.<br/>
+                        <br/>
+                    It includes controlling physical access to the hardware, as well as protecting against harm that may come via network access, data and code injection, and due to malpractice by operators, whether intentional, accidental, or due to them being tricked into deviating from secure procedures.
+                </h5>
+       `
+   
+   
+};
+
+function createTemplate (data){
+    
+  var title = "data.title";
+  var heading ="data.heading";
+  var date ="data.date";
+  var content ="data.content";
+
+
+var htmlTemplate =`
+        <!doctype html>
+<html>
+    <head>
+        <title>${title}</title>
+       	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+       	<link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body  class="body">
+     <div>
+         <a href="/">HOME</a>
+         
+     </div> 
+     <hr/>
+     <div>
+         ${heading}
+     </div>
+     <div>
+        ${date}
+     </div>
+     <div>
+     
+        ${content}
+     </div>   
+        
+        
+    </body>
+    
+</html>
+
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req,res){
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createTemplate(articleone));
 });
 
 app.get('/article-two', function(req,res){
