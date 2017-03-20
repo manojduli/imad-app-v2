7f5693c75a48a4d502bb282855233b8e372e8c3b
manoj duli <manojduli123@gmail.com>
2017-03-20 23:43:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ var article={
                 <h1 class="body"> <i>Advantages of Cyber security:</i></h1>
                    <h5>
                     <ul id="heads">
-                      <li>Protects networks and resources.</h3> </li>
-                       <li><h3>Protects  against data from theft.</h3> </li>
+                      <li><h3>Protects networks and resources.</h3></li>
+                       <li><h3>Protects  against data from theft.</h3></li>
                        <li><h3>Increase in cyber defense. </h3></li>
                        <li><h3>Increase in cyber speed. </h3></li>
-                       <li><h3>Increases the security.</h3> </li>
+                       <li><h3>Increases the security.</h3></li>
                        <li><h3>Protects systems and computers against virus worms and malware.</h3> </li>
                        <li><h3>Browse the safe web site.</h3></li>
                     </ul>
