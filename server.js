1440792dfc058f8a1625e207ed4a562c146b5f01
manoj duli <manojduli123@gmail.com>
2017-03-21 00:23:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ var article={
             <body >
             
                <h1  class="body"> <i>Cyber security:</i></h1>
-             <h1 class="body"> <i>Advantages of Cyber security:</i></h1>
+             <h1 class="body"> <i>Facts of Cyber security:</i></h1>
                 <ul id="heads" >
                     <li><h3>	For every second, 18 members are becoming a victims of cyber-crime.</h3></li>
                     <li><h3>	India ranks in the 5th position in cyber-crimes with 16.9%.</h3></li>
